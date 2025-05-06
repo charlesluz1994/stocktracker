9b60bfe9e7e3c73d7f3683977f900b06853b3be7
charlesluz1994 <charlesluz.94@gmail.com>
2025-05-06 16:18:50 -0300

    Create structure, and first configurations of the application. Insert controller with endpoints, brapi configuration with feign client, cache.

---
+++ src/main/java/com/cluz/stocktracker/StocktrackerApplication.java
@@ -0,0 +1,16 @@
+package com.cluz.stocktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableCaching
+public class StocktrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocktrackerApplication.class, args);
+	}
+}
